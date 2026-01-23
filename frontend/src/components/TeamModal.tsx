add6061d2cea454fae0070b792b65fdbb9f32a9f
Cordeiro Mariano <cordeiromariano17@gmail.com>
2026-01-23 09:57:18 -0300

    publics calls fixed

--- frontend/src/components/TeamModal.tsx
+++ frontend/src/components/TeamModal.tsx
@@ -16,11 +16,11 @@ export interface TeamModalProps {
   players: TeamPlayer[];
 }
 
-import topIcon from '../images/top.png';
-import jgIcon from '../images/jg.png';
-import midIcon from '../images/mid.png';
-import adcIcon from '../images/adc.png';
-import suppIcon from '../images/supp.png';
+import topIcon from '/images/top.png';
+import jgIcon from '/images/jg.png';
+import midIcon from '/images/mid.png';
+import adcIcon from '/images/adc.png';
+import suppIcon from '/images/supp.png';
 
 const roleImages: Record<string, string> = {
   TOP: topIcon,
