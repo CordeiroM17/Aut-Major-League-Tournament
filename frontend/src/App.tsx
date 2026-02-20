4fa088670d96542824a75c5161ab5f226dc8fc16
Cordeiro Mariano <cordeiromariano17@gmail.com>
2026-02-20 13:46:30 -0300

    Vercel Stadistics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/next";
+import { TournamentPage } from "./pages/TournamentPage";
+import { PlayoffsPage } from "./pages/PlayoffsPage";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { MainPage } from "./pages/MainPage";
+import { ReglamentoPage } from "./pages/ReglamentoPage";
+import { DiscordPage } from "./pages/DiscordPage";
+import { AdminPage } from "./pages/AdminPage";
 
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { TournamentPage } from './pages/TournamentPage';
-import { PlayoffsPage } from './pages/PlayoffsPage';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import { MainPage } from './pages/MainPage';
-import { ReglamentoPage } from './pages/ReglamentoPage';
-import { DiscordPage } from './pages/DiscordPage';
-import { AdminPage } from './pages/AdminPage';
-
-import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from "./components/ScrollToTop";
 
 const App: React.FC = () => {
   return (
@@ -26,6 +26,7 @@ const App: React.FC = () => {
         <Route path="/discord" element={<DiscordPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 };
